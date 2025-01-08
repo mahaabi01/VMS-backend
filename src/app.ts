5d73912b1facfc8919530bf380dd601fe1b4b4d8
mahaabi01 <mahaabi01@gmail.com>
2025-01-08 12:14:00 +0545

    database connection code

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express"
+const app:Application = express()
+const PORT:number = 3000
+
+
+require('./model/index') //db connection code
+
+
+app.get("/", (req:Request,res:Response)=>{
+  res.send("Project Started.")
+})
+
+app.listen(PORT,()=>{
+  console.log("Server has started at port,", PORT)
+})
